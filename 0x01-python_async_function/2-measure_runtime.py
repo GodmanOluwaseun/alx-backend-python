215237f5f3445b85aba9d23bda54eaf19cd9c96d
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-10-17 17:25:03 +0000

    Measures total time for executing Async coroutines and returns the time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""2-measure_runtime
+Measures total time for executing Async coroutines and returns the time.
+"""
+
+
+import asyncio
+from time import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures runtime of coroutines & returns average time for each"""
+    
+    start = time()
+    await wait_n(n, max_delay)
+    stop = time()
+
+    total_time = stop - start
+    time_n = total_time / n
+    return time_n
